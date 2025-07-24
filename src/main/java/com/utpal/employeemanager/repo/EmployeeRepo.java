e69526af6e7e137649f7703147b079e10bccaffe
utpal-kumar-singh <utpalsingh2002@gmail.com>
2025-07-24 08:03:22 +0530

    i m creating service model

--- src/main/java/com/utpal/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/utpal/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.utpal.employeemanager.repo;
 import com.utpal.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
--- src/main/java/com/utpal/employeemanager/service/EmployeeService.java
+++ src/main/java/com/utpal/employeemanager/service/EmployeeService.java
@@ -1,14 +1,38 @@
 package com.utpal.employeemanager.service;
 
+import com.utpal.employeemanager.exception.UserNotFoundException;
+import com.utpal.employeemanager.model.Employee;
 import com.utpal.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class EmployeeService {
-    private final EmployeeRepo EmployeeRepo;
+    private final EmployeeRepo employeeRepo;
 @Autowired
     public EmployeeService(EmployeeRepo employeeRepo) {
-        EmployeeRepo = employeeRepo;
+       this.employeeRepo = employeeRepo;
     }
+    public Employee addEmployee(Employee employee){
+    employee.setEmployeeCode(UUID.randomUUID().toString());
+    return employeeRepo.save(employee);
+    }
+    public List<Employee> findAllEmployee(){
+    return employeeRepo .findAll();
+
+    }
+
+    public Employee updateEmployee(Employee employee){
+    return employeeRepo.save(employee);
+    }
+    public Employee findEmployeeBId(Long id){
+    return employeeRepo.findEmployeeById(id).orElseThrow(() ->new UserNotFoundException("user by id"+ id +"was not found"));
+    }
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
 }
